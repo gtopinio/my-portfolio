dc767e9422692031282b337f762aa0a3db32f16d
gtopinio <mctopinio@up.edu.ph>
2023-07-15 00:15:37 +0800

    update: add appealing styles to profile and content components including svg's, a navbar, custom fonts and additional tailwind properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
     extend: {
       colors: {
         'smokey-black': '#0F0F0F',
+        'mirage-gray': '#1e1e20',
+        'gray-container': '#2b2b2d',
       },
       fontFamily: {
         body: ['Poppins'],
