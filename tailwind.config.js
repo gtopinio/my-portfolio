3e67893339d8d97738f85c486d9af7232ab9c0aa
gtopinio <mctopinio@up.edu.ph>
2023-07-14 16:32:55 +0800

    update: add smokey-black color to tailwind config and body background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'smokey-black': '#0F0F0F',
+      }
+    },
   },
   plugins: [],
 }
