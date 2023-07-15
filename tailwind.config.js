05bb8e001da118a604adcc1e3b5502a35db97158
gtopinio <mctopinio@up.edu.ph>
2023-07-15 14:50:46 +0800

    update: add style to contacs and address details

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
         'smokey-black': '#0F0F0F',
         'mirage-gray': '#1e1e20',
         'gray-container': '#2b2b2d',
+        'card-container':'#202022',
+        'outer-space': '#3a3a3a',
       },
       fontFamily: {
         body: ['Poppins'],
