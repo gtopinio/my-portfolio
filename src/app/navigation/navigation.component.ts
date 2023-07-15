3dfb9b16f3043c56530e31c87e7ccf66f82960c7
gtopinio <mctopinio@up.edu.ph>
2023-07-15 21:13:45 +0800

    update: remove unused variale inside navigation class

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 })
 export class NavigationComponent {
   currentTab: string = 'About';
-  activeTab = 'text-white bg-blue';
   navTabs: string[] = ['About','Resume', 'Projects', 'Contact'];
 
   isActive(tab: string): boolean {
