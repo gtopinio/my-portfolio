1d0e1aaa8bfd8475870c681a74e9120b890df8cd
gtopinio <mctopinio@up.edu.ph>
2023-07-16 00:41:16 +0800

    update: add navigation header with style beside navigation component

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 export class NavigationComponent {
   currentTab: string = 'About';
   navTabs: string[] = ['About','Resume', 'Projects', 'Contact'];
+  @Output() public navigationEvent = new EventEmitter();
 
   isActive(tab: string): boolean {
     return this.currentTab === tab;
@@ -16,5 +17,11 @@ export class NavigationComponent {
 
   changeTab(tab: string): void {
     this.currentTab = tab;
+
+    if(tab == 'About') {
+      this.navigationEvent.emit(this.currentTab + ' Me');
+    } else {
+      this.navigationEvent.emit(this.currentTab);
+    }
   }
 }
