884ce85c1c0da950b14c4ee69302a2d43058e52b
gtopinio <mctopinio@up.edu.ph>
2023-07-17 18:04:31 +0800

    update: add additional courses to resume component

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -65,11 +65,175 @@ export class CourseService {
         "school": "University of the Philippines Los Baños",
         "year": "2nd Semester 2022-2023",
         "activities": [
-          `Designed and implemented a server-client Java program for a Parallel Computing course, utilizing open
+          `Designed and implemented a server-client Java program utilizing open
           sockets to establish efficient data communication between multiple instances, facilitating parallel
           processing and efficient computation for complex tasks in distributed computing environments.`,
           `Learned about the principles and techniques of parallel computing, parallel computational models,
            machine architectures, and performance models.`,
+        ]
+      },
+      {
+        "id": "CMSC 170",
+        "name": "Introduction to Artificial Intelligence",
+        "school": "University of the Philippines Los Baños",
+        "year": "1st Semester 2022-2023",
+        "activities": [
+          `Successfully completed a range of exercises in an Introduction to Artificial Intelligence course,
+          demonstrating proficiency in developing a user interface for an Eight-Puzzle game and implementing
+          algorithms like BFS, DFS, A*, and MinMax to solve various AI problems.`,
+        ]
+      },
+      {
+        "id": "CMSC 142",
+        "name": "Automata and Language Theory",
+        "school": "University of the Philippines Los Baños",
+        "year": "1st Semester 2022-2023",
+        "activities": [
+          `Analyzed and designed finite automata, context-free grammars, and pushdown automata`,
+        ]
+      },
+      {
+        "id": "CMSC 132",
+        "name": "Computer Architecture",
+        "school": "University of the Philippines Los Baños",
+        "year": "1st Semester 2022-2023",
+        "activities": [
+          `Explored computer architecture, analyzing system performance and studying topics like
+          instruction sets, memory management, and input/output systems.`,
+        ]
+      },
+      {
+        "id": "CMSC 125",
+        "name": "Operating Systems",
+        "school": "University of the Philippines Los Baños",
+        "year": "1st Semester 2022-2023",
+        "activities": [
+          `Learned about operating systems, particularly Linux,
+          including topics such as process and memory management, file systems, and security.`,
+        ]
+      },
+      {
+        "id": "CMSC 124",
+        "name": "Design and Implementation of Programming Languages",
+        "school": "University of the Philippines Los Baños",
+        "year": "1st Semester 2022-2023",
+        "activities": [
+          `Built language interpreters or compilers, gaining hands-on experience with language
+          implementation.`,
+        ]
+      },
+      {
+        "id": "CMSC 131",
+        "name": "Introduction to Computer Organization and Machine-Level Programming",
+        "school": "University of the Philippines Los Baños",
+        "year": "2nd Semester 2021-2022",
+        "activities": [
+          `Experienced in machine-level programming and debugging using assembly language and tools.`,
+        ]
+      },
+      {
+        "id": "CMSC 127",
+        "name": "File Processing and Database Systems",
+        "school": "University of the Philippines Los Baños",
+        "year": "2nd Semester 2021-2022",
+        "activities": [
+          `Explored topics such as data models, database design, and SQL queries.`,
+          `Developed a task record system application integrated with CRUD operations and an SQL database`,
+        ]
+      },
+      {
+        "id": "CMSC 100",
+        "name": "Web Programming",
+        "school": "University of the Philippines Los Baños",
+        "year": "2nd Semester 2021-2022",
+        "activities": [
+          `Learned about the principles and techniques of web programming and explored topics such as HTML, CSS, and JavaScript`,
+          `Developed a social media web app with CRUD operations using ReactJS and ExpressJS`,
+        ]
+      },
+      {
+        "id": "CMSC 23",
+        "name": "Mobile Computing",
+        "school": "University of the Philippines Los Baños",
+        "year": "2nd Semester 2021-2022",
+        "activities": [
+          `Learned about mobile application development, mobile operating systems, and mobile networking.`,
+          `Developed a mobile social media app using Flutter with SQLite`,
+        ]
+      },
+      {
+        "id": "CMSC 150",
+        "name": "Numerical and Symbolic Computation",
+        "school": "University of the Philippines Los Baños",
+        "year": "1st Semester 2021-2022",
+        "activities": [
+          `Developed a stand-alone application using R with a user interface for the Quadratic Spline
+          Interpolation and the Simplex Method`,
+        ]
+      },
+      {
+        "id": "CMSC 130",
+        "name": "Logic Design and Digital Computer Circuits",
+        "school": "University of the Philippines Los Baños",
+        "year": "1st Semester 2021-2022",
+        "activities": [
+          `Experienced digital circuit design, including number systems, Boolean algebra, and sequential circuits.`,
+        ]
+      },
+      {
+        "id": "CMSC 123",
+        "name": "Data Structures",
+        "school": "University of the Philippines Los Baños",
+        "year": "1st Semester 2021-2022",
+        "activities": [
+          `Experienced in implementing data structures and selecting appropriate algorithms for programming projects.`,
+        ]
+      },
+      {
+        "id": "CMSC 22",
+        "name": "Object-Oriented Programming",
+        "school": "University of the Philippines Los Baños",
+        "year": "1st Semester 2021-2022",
+        "activities": [
+          `Learned about object-oriented design principles, classes and objects, inheritance, polymorphism, and encapsulation`,
+          `Developed a mini-shooting game using Java.`,
+        ]
+      },
+      {
+        "id": "CMSC 57",
+        "name": "Discrete Mathematical Structures in Computer Science II",
+        "school": "University of the Philippines Los Baños",
+        "year": "Midyear 2021",
+        "activities": [
+          `Explored discrete mathematics and its connection to computer science,
+          covering graph theory, algorithms, and formal languages.`,
+        ]
+      },
+      {
+        "id": "CMSC 21",
+        "name": "Fundamentals of Programming",
+        "school": "University of the Philippines Los Baños",
+        "year": "Midyear 2021",
+        "activities": [
+          `Learned problem-solving, algorithm design, and C programming skills.`,
+        ]
+      },
+      {
+        "id": "CMSC 56",
+        "name": " Discrete Mathematical Structures in Computer Science I",
+        "school": "University of the Philippines Los Baños",
+        "year": "2nd Semester 2020-2021",
+        "activities": [
+          `Enhanced critical thinking and problem-solving skills through mathematical projects and assignments.`,
+        ]
+      },
+      {
+        "id": "CMSC 12",
+        "name": "Foundations of Computer Science",
+        "school": "University of the Philippines Los Baños",
+        "year": "2nd Semester 2020-2021",
+        "activities": [
+          `Developed a shopping app project with product, sale, and seller-related functions using Python`,
         ]
       },
       ]
