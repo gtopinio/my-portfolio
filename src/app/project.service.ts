496d59e86352ee4b7351331ce3a6f3aa9db9eaca
gtopinio <mctopinio@up.edu.ph>
2023-07-21 14:19:41 +0800

    update: change placeholder project detail values from project service

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -11,23 +11,23 @@ export class ProjectService {
       id: 1,
       header: 'ELBNB',
       subheader: 'Student Accommodation and Lodging System',
-      description: ' A web application that allows students to find accommodation and lodging within or outside the campus of University of the Philippines Los Baños.',
+      description: 'A web application that allows students to find accommodation and lodging within or outside the campus of University of the Philippines Los Baños.',
       image: 'assets/images/default-project-image.png',
       link: 'https://github.com/gtopinio/elbnb-backend-128'
       },
       {
-        id: 1,
-        header: 'ELBNB',
-        subheader: 'Student Accommodation and Lodging System',
-        description: ' A web application that allows students to find accommodation and lodging within or outside the campus of University of the Philippines Los Baños.',
+        id: 2,
+        header: 'Elemental Odyssey ',
+        subheader: 'Networked Java Game',
+        description: 'A 2D multiplayer platformer game that allows players to choose a character with unique abilities and race against each other to reach the end of the level.',
         image: 'assets/images/default-project-image.png',
         link: 'https://github.com/gtopinio/elbnb-backend-128'
       },
       {
-        id: 1,
-        header: 'ELBNB',
-        subheader: 'Student Accommodation and Lodging System',
-        description: ' A web application that allows students to find accommodation and lodging within or outside the campus of University of the Philippines Los Baños.',
+        id: 3,
+        header: 'Partition Problem Solver',
+        subheader: ' Iterative Backtracking Algorithm',
+        description: 'A C program that uses a modified backtracking method to efficiently solve the Partition problem.',
         image: 'assets/images/default-project-image.png',
         link: 'https://github.com/gtopinio/elbnb-backend-128'
       },
