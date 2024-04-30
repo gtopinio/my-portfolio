ff4b0fcd21e4a3eb6913feb65ca5514172be4b08
gtopinio <mctopinio@up.edu.ph>
2024-04-30 16:00:34 +0800

    revert: back to stable again

---
+++ mynode.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+const dotenv = require('dotenv').config({path: 'src/.env'});
+
+const envFile = `export const environment = {
+    HTTP_SMS_KEY: '${process.env.HTTP_SMS_KEY}',
+    MY_PHONE_NUMBER: '${process.env.MY_PHONE_NUMBER}',
+};
+`;
+const targetPath = path.join(__dirname, './src/environments/environment.development.ts');
+fs.writeFile(targetPath, envFile, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
+  }
+});
