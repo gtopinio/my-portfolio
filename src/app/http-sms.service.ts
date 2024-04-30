97f8941e18db28a09bac43f203e8b9815df17eb1
gtopinio <mctopinio@up.edu.ph>
2024-04-30 15:45:47 +0800

    feature: configure environment vars; new httpsms service for contact component

---
+++ src/app/http-sms.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { environment } from "../environments/environment.development";
+import HttpSms from 'httpsms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpSmsService {
+
+  _client: HttpSms;
+  myPhoneNumber = environment.MY_PHONE_NUMBER;
+
+  constructor() {
+    this._client = new HttpSms(environment.HTTP_SMS_KEY);
+  }
+
+  async sendSms(name: string, email: string, message: string) : Promise<boolean> {
+    let isSent = false;
+    message = '' +
+      'My Portfolio Notification: \n\n' +
+      'Name: ' + name + '\nEmail: ' + email + '\nMessage: '
+      + message + '\n\nThis is an automated message. Do not reply.';
+
+    await this._client.messages.postSend({
+      content: message,
+      from: this.myPhoneNumber,
+      to: this.myPhoneNumber,
+      encrypted: false
+    })
+    .then(() => {
+      console.log('Successfully sent the SMS');
+      isSent = true;
+    })
+    .catch((error) => {
+      console.error('Failed to send the SMS');
+      console.error(error);
+      isSent = false;
+    });
+
+    return isSent;
+  }
+
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  HTTP_SMS_KEY: '',
+  MY_PHONE_NUMBER: '',
+}
